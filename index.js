b9b9be60d80cde694ca9ef01f5d88ae761b2a221
V I B E <115653174+Official0VIBE@users.noreply.github.com>
2022-10-29 09:53:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,29 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds, 
-		GatewayIntentBits.GuildMessages, 
-		GatewayIntentBits.GuildPresences, 
-		GatewayIntentBits.GuildMessageReactions, 
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent
-	], 
+	intents: [ GatewayIntentBits.Guilds,  GatewayIntentBits.GuildMessages,  GatewayIntentBits.GuildPresences,  GatewayIntentBits.GuildMessageReactions,  GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent ], 
 	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
 });
-
 const fs = require('fs');
 const config = require('./config.json');
 require('dotenv').config() // احذف السطر ذا اذا تستخدم ربلت
-
 client.commands = new Collection()
 client.aliases = new Collection()
 client.slashCommands = new Collection();
 client.buttons = new Collection();
 client.prefix = config.prefix;
-
 module.exports = client;
-
-
 fs.readdirSync('./handlers').forEach((handler) => {
   require(`./handlers/${handler}`)(client)
 });
-
-console.log(`▏ FNLBase » Connecting to the database...`)
-
+console.log(`▏ GinixBase » Connecting to the database...`)
 const MongoDB = require('mongodb');
-
 const Mongoose = require('mongoose');
-
-console.log(`▏ FNLBase » Fetching the (URI)`);
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = config.MONGODB.URI;
-console.log(`▏ FNLBase » (URI) Has been fetched\n▏ URI » ${uri}`);
-
-
-const dbclient = new MongoDB.MongoClient(uri, {
-	useNewUrlParser: true, useUnifiedTopology: true, serverApi: MongoDB.ServerApiVersion.v1
-})
-
+const dbclient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 try {
 	dbclient.connect()
-	console.log(`▏ FNLBase » Database has been connected`);
+	console.log(`▏ MixedBase » Database has been connected`);
 } catch (error) {
   console.log(error);
 }
